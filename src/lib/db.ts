d63a1bf707f4c4a6edb706a0c439d68118059b5f
Shrey31G <gangwarshrey.31@gmail.com>
2025-11-22 18:16:47 +0530

    fixing singleton

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-    prisma?: PrismaClient;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-    globalForPrisma.prisma ??
+    globalForPrisma.prisma ||
     new PrismaClient({
-        log: ["error", "warn"],
+        log: ["query"],
     });
 
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
-}
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
